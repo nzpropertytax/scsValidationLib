4f2b84293c3b356018ebe173dda998adcbaaad9e
nzpropertytax <71737995+nzpropertytax@users.noreply.github.com>
2026-05-18 14:45:14 +1200

    Add files via upload

---
+++ scsValidationLib/Areas/Validation/ValidationAttributes.cs
@@ -0,0 +1,273 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+
+namespace scsValidationLib.Areas.Validation
+{
+    public static class ValidationAttributes
+    {
+
+        public class NotZeroValidation : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+            {
+                double doubleValue = Convert.ToDouble(value);
+                if (Math.Abs(doubleValue) > double.Epsilon)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult("The value cannot be zero.", new string[] { validationContext.MemberName });
+                }
+            }
+        }
+
+        public class RequireIfAnotherPropertyIsNotZero : ValidationAttribute
+        {
+            private readonly string[] _dependentProperties;
+
+            public RequireIfAnotherPropertyIsNotZero(params string[] dependentProperties)
+            {
+                _dependentProperties = dependentProperties;
+            }
+
+            protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+            {
+                var memberDisplayName = validationContext.DisplayName ?? validationContext.MemberName;
+
+                foreach (string dependentProperty in _dependentProperties)
+                {
+                    var dependentPropertyValue = validationContext.ObjectInstance.GetType().GetProperty(dependentProperty)?.GetValue(validationContext.ObjectInstance);
+
+                    var dependentPropertyDisplayName = validationContext.ObjectType.GetProperty(dependentProperty)?.GetCustomAttributes(typeof(DisplayAttribute), true).Cast<DisplayAttribute>().SingleOrDefault()?.GetName() ?? dependentProperty;
+
+                    if (dependentPropertyValue is null)
+                    {
+                        continue;
+                    }
+
+                    if (!double.TryParse(dependentPropertyValue.ToString(), out double dependentPropertyDoubleValue))
+                    {
+                        return new ValidationResult($"The dependent property {dependentProperty} must be a double.", new string[] { dependentProperty });
+                    }
+
+                    if (dependentPropertyDoubleValue != 0 && (value == null || (decimal)value == 0))
+                    {
+                        return new ValidationResult($"If {dependentPropertyDisplayName} is not zero, {memberDisplayName} must have a value.", new string[] { validationContext.MemberName });
+                    }
+                }
+
+                return ValidationResult.Success;
+            }
+
+
+        }
+
+        public class NotBeforeYear1900Validation : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+            {
+                DateTime date = (DateTime)value;
+                DateTime year1900 = new DateTime(1990, 1, 1);
+
+                if (date >= year1900)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult($"{validationContext.DisplayName} cannot be before the year 1900.", new string[] { validationContext.MemberName });
+                }
+            }
+        }
+
+        public class NotFutureDateValidation : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+            {
+                DateTime date = (DateTime)value;
+                if (date <= DateTime.Now)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult($"{validationContext.DisplayName} cannot be in the future.", new string[] { validationContext.MemberName });
+                }
+            }
+        }
+
+        public class NotNegative : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+            {
+                if (value == null)
+                {
+                    return new ValidationResult($"{validationContext.DisplayName} cannot be null.", new string[] { validationContext.MemberName });
+                }
+
+                if ((value is decimal decVal && decVal >= 0m) || (value is int intVal && intVal >= 0))
+                {
+                    return ValidationResult.Success;
+                }
+
+                if (!(value is decimal || value is int))
+                {
+                    return new ValidationResult("Invalid type.", new string[] { validationContext.MemberName });
+                }
+
+                return new ValidationResult($"{validationContext.DisplayName} cannot be negative.", new string[] { validationContext.MemberName });
+            }
+        }
+
+
+        public class NotNegativeButCanBeNull : ValidationAttribute //TODO: Change to NotNegative ?
+        {
+            protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+            {
+                if (value == null)
+                {
+                    return ValidationResult.Success;
+                }
+                decimal val = (decimal)value;
+                if (value != null && val >= 0)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult($"{validationContext.DisplayName} cannot be negative.", new string[] { validationContext.MemberName });
+                }
+            }
+        }
+
+        public class IRDValidation : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+            {
+                if (validationContext != null)
+                {
+                    if (value != null)
+                    {
+                        string pattern = @"^(\d{3}-\d{3}-\d{3})";
+                        string valueString = value.ToString();
+
+                        bool irdFormatMatch = System.Text.RegularExpressions.Regex.IsMatch(valueString, pattern);
+
+                        if (!irdFormatMatch)
+                        {
+                            return new ValidationResult("Invalid IRD number. Must be: xxx-xxx-xx format.", new string[] { validationContext.MemberName });
+                        }
+                    }
+                    return ValidationResult.Success;
+                }
+                throw new ArgumentNullException(nameof(validationContext));
+            }
+        }
+        public class PercentValidation : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+            {
+                if (validationContext != null)
+                {
+                    if (value != null)
+                    {
+                        int v = 0;
+                        try
+                        {
+                            v = int.Parse(value.ToString());
+                        }
+                        catch
+                        {
+                            return new ValidationResult("Invalid Percentage Value.", new string[] { validationContext.MemberName });
+                        }
+                        if (v < 0 | v > 100)
+                        {
+                            return new ValidationResult("Invalid Percentage Value.", new string[] { validationContext.MemberName });
+                        }
+                    }
+                    return ValidationResult.Success;
+                }
+                throw new ArgumentNullException(nameof(validationContext));
+            }
+        }
+
+        public enum CompareOperator
+        {
+            GreaterThan,
+            GreaterThanOrEqualTo,
+            LessThan,
+            LessThanOrEqualTo,
+            EqualTo,
+            NotEqualTo
+        }
+
+        public class CompareDateValidation : ValidationAttribute
+        {
+            private readonly string _dependentPropertyName;
+            private readonly CompareOperator _operatorType;
+
+            public CompareDateValidation(string dependentPropertyName, CompareOperator operatorType)
+            {
+                _dependentPropertyName = dependentPropertyName;
+                _operatorType = operatorType;
+            }
+
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                DateTime date = (DateTime)value;
+                PropertyInfo dependentProperty = validationContext.ObjectType.GetProperty(_dependentPropertyName);
+
+                if (dependentProperty == null)
+                {
+                    return new ValidationResult($"Property '{_dependentPropertyName}' not found.");
+                }
+
+                var dependentPropertyDisplayName = validationContext.ObjectType.GetProperty(_dependentPropertyName)?.GetCustomAttributes(typeof(DisplayAttribute), true)
+                    .Cast<DisplayAttribute>()
+                    .SingleOrDefault()?.GetName();
+
+                DateTime dependentDate = (DateTime)dependentProperty.GetValue(validationContext.ObjectInstance);
+
+                bool isValid;
+
+                switch (_operatorType)
+                {
+                    case CompareOperator.GreaterThan:
+                        isValid = date > dependentDate;
+                        break;
+                    case CompareOperator.GreaterThanOrEqualTo:
+                        isValid = date >= dependentDate;
+                        break;
+                    case CompareOperator.LessThan:
+                        isValid = date < dependentDate;
+                        break;
+                    case CompareOperator.LessThanOrEqualTo:
+                        isValid = date <= dependentDate;
+                        break;
+                    case CompareOperator.EqualTo:
+                        isValid = date == dependentDate;
+                        break;
+                    case CompareOperator.NotEqualTo:
+                        isValid = date != dependentDate;
+                        break;
+                    default:
+                        isValid = false;
+                        break;
+                }
+
+                if (isValid)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    string operatorTypeString = ValidationHelper.GetFormattedEnumString(_operatorType);
+                    return new ValidationResult($"{validationContext.DisplayName} must be {operatorTypeString}: {dependentPropertyDisplayName}", new string[] { validationContext.MemberName });
+                }
+            }
+        }
+
+    }
+}
---
+++ scsValidationLib/Areas/Validation/ValidationHelper.cs
@@ -0,0 +1,203 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text;
+
+namespace scsValidationLib.Areas.Validation
+{
+    public static class ValidationHelper
+    {
+        public static class ValidationStatus
+        {
+            public const string Valid = "Valid";
+            public const string Invalid = "InValid";
+        }
+
+        public class MultipleValidationResult : ValidationResult
+        {
+            private readonly List<ValidationResult> _validationResults;
+
+            public MultipleValidationResult(string errorMessage, List<ValidationResult> validationResults)
+                : base(errorMessage)
+            {
+                _validationResults = validationResults;
+            }
+
+            public IEnumerable<ValidationResult> ValidationResults
+            {
+                get { return _validationResults; }
+            }
+        }
+
+        public class ValidationWarningResult : ValidationResult
+        {
+            public ValidationWarningResult(string warningMessage, IEnumerable<string>? memberNames) : base(warningMessage, memberNames) { }
+            public ValidationWarningResult(string warningMessage) : base(warningMessage) { }
+        }
+
+        public static List<ValidationResult> ValidateModel<T>(T model, ICollection<ValidationResult>? validationResults = null)
+        {
+            try
+            {
+                //if existing validation results are not passed in then create a new list
+                validationResults ??= new List<ValidationResult>();
+
+                if (model == null)
+                {
+                    throw new ArgumentNullException(nameof(model));
+                }
+
+                var validationContext = new ValidationContext(model);
+
+                //validationResults is being added to by the TryValidateObject method
+                if (!Validator.TryValidateObject(model, validationContext, validationResults, true))
+                {
+                    foreach (var result in validationResults.ToList())
+                    {
+                        //if there are multiple validations then add them into the list
+                        if (result is MultipleValidationResult multipleValidationResult)
+                        {
+                            foreach (var innerValidationResult in multipleValidationResult.ValidationResults)
+                            {
+                                //if the exact same error message already exists for the same field then don't add it.
+                                if (!validationResults.Any(x => x.ErrorMessage == innerValidationResult.ErrorMessage))
+                                {
+                                    validationResults.Add(innerValidationResult);
+                                }
+                            }
+                            //remove the result that contained the inner results
+                            validationResults.Remove(result);
+                        }
+                    }
+                    return (List<ValidationResult>)validationResults;
+                }
+                return (List<ValidationResult>)validationResults;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error while validating model '{typeof(T).Name}': {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// A generic method to validate a property using a specified validation attribute.
+        /// </summary>
+        /// <typeparam name="TValidation">The type of the validation attribute to apply (must inherit from ValidationAttribute).</typeparam>
+        /// <param name="validationErrors">A list of validation errors to update with any new validation errors.</param>
+        /// <param name="value">The value of the property to be validated.</param>
+        /// <param name="fieldId">The identifier of the field being validated.</param>
+        /// <param name="displayName">An optional display name for the field being validated. Defaults to an empty string.</param>
+        /// <returns>A list of validation errors, updated with any new errors resulting from the specified validation.</returns>
+        /// <exception cref="ArgumentException">Thrown when the TValidation type does not inherit from ValidationAttribute.</exception>
+        ///
+        /// Usage example:
+        /// validationErrors = ValidateProperty<NotFutureDateValidation>(validationErrors, date, fieldID, displayName);
+        /// validationErrors = ValidateProperty<NotBeforeYear1900Validation>(validationErrors, date, fieldID, displayName);
+
+        public static List<ValidationResult> ValidateProperty<TValidation>(List<ValidationResult> validationErrors, object value, string fieldId, string? displayName = "")
+        {
+            var validator = Activator.CreateInstance<TValidation>() as ValidationAttribute;
+            if (validator == null)
+            {
+                throw new ArgumentException("TValidation must inherit from ValidationAttribute.");
+            }
+
+            var context = new ValidationContext(value, null, null) { MemberName = fieldId };
+            context.DisplayName = displayName;
+            var validationResult = validator.GetValidationResult(value, context);
+
+            if (validationResult != ValidationResult.Success)
+            {
+                validationErrors.Add(validationResult);
+            }
+
+            return validationErrors;
+        }
+
+        private static List<ValidationResult> ValidateField<T>(T model, string fieldName, List<ValidationResult>? validationResults = null)
+        {
+            validationResults ??= new List<ValidationResult>();
+
+            var field = model.GetType().GetProperty(fieldName);
+            if (field != null)
+            {
+                var value = field.GetValue(model);
+                var validationAttribute = field.GetCustomAttributes<ValidationAttribute>();
+                foreach (var attribute in validationAttribute)
+                {
+                    // Create a new validation context with the necessary properties
+                    var validationContext = new ValidationContext(model)
+                    {
+                        MemberName = fieldName,
+                        DisplayName = field.Name
+                    };
+                    ValidationResult validationResult = attribute.GetValidationResult(value, validationContext);
+                    if (validationResult != ValidationResult.Success)
+                    {
+                        validationResults.Add(validationResult);
+                    }
+                }
+            }
+            return validationResults;
+        }
+
+        //Exclusively valitate these fields passed in
+        public static List<ValidationResult> ValidateFields<T>(T model, string[] fieldNames, List<ValidationResult>? validationResults = null)
+        {
+            validationResults ??= new List<ValidationResult>();
+            foreach (var fieldName in fieldNames)
+            {
+                validationResults = ValidateField(model, fieldName, validationResults);
+            }
+            return validationResults;
+        }
+
+        public static string GetFieldDisplayName(string FieldName, Type ModelType)
+        {
+            var field = ModelType.GetProperty(FieldName);
+
+            if (field != null)
+            {
+                var displayAttribute = field.GetCustomAttribute(typeof(DisplayAttribute)) as DisplayAttribute;
+
+                //if null return passed in fieldName
+                if (displayAttribute == null)
+                {
+                    return FieldName;
+                }
+
+                return displayAttribute.Name;
+            }
+
+            return FieldName;
+        }
+
+
+        public static string GetFormattedEnumString(Enum value)
+        {
+            string enumString = value.ToString();
+            StringBuilder formattedString = new StringBuilder();
+
+            for (int i = 0; i < enumString.Length; i++)
+            {
+                if (i > 0 && char.IsUpper(enumString[i]))
+                {
+                    formattedString.Append(' ');
+                }
+                formattedString.Append(char.ToLower(enumString[i]));
+            }
+
+            return formattedString.ToString();
+        }
+        public static string FormatErrorMessage(IList<ValidationResult> validation)
+        {
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in validation)
+                {
+                    sb.AppendFormat("{0}: {1}, ", item.MemberNames, item.ErrorMessage);
+                }
+                return sb.ToString().TrimEnd(',', ' ');
+        }
+
+    }
+
+}
